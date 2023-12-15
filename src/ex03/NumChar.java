b79e1a6331d4d24972eec931bcb8ee8e333c092f
seoldonghun <ehdgns5647@naver.com>
2023-12-15 13:05:49 +0900

    숫자 문자 출력

---
+++ src/ex03/NumChar.java
@@ -0,0 +1,36 @@
+package ex03;
+
+import java.util.Scanner;
+
+/**
+ * 입력한 정수를 영어로 출력하는 프로그램을 만들기 ( 조건문 활용 )
+ * 1 부터 9 사이가 아니면 "OTHER"를 출력
+ */
+public class NumChar {
+    public static void main(String[] args) {
+        // 1. 숫자를 입력 받도록 만든다.
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        //System.out.println(num);
+        // 2. 숫자를 입력시 영어가 출력되도록 만드시오.
+        String[] eNum = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+
+        for (int i = 0; i < 9; i++) {
+            if (num == (i + 1)) {
+                System.out.println(eNum[i]);
+            }
+        }
+        if (num >= 10 || num <= 0)
+            System.out.println("OTHER");
+
+        // 모듈화 전 과정
+//        if(num == 1){
+//            System.out.println(eNum[0]);
+//        }
+//
+//        if(num == 2){
+//            System.out.println(eNum[1]);
+//        }
+        
+    }
+}
