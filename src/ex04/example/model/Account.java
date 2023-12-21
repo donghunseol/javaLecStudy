4f1b7514358feeb422c2287518fe5e699a0a635e
seoldonghun <ehdgns5647@naver.com>
2023-12-21 11:47:26 +0900

    계좌이체 절차적 테스트 완료

--- src/ex04/example/model/Account.java
+++ src/ex04/example/model/Account.java
@@ -1,9 +1,9 @@
 package ex04.example.model;
 
 public class Account {
-    final int id; // 계좌번호 (숫자 4자리로 만들어져있다 -> 제약조건)
-    long balance;
-    int userId;
+    public final int id; // 계좌번호 (숫자 4자리로 만들어져있다 -> 제약조건)
+    public long balance;
+    public int userId;
 
 
     public Account(int id, long balance, int userId) {
