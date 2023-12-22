3a814103a27cf946a40e9246721f14a5c6879c02
seoldonghun <ehdgns5647@naver.com>
2023-12-22 10:14:21 +0900

    상속과 생성자

---
+++ src/ex06/Employee.java
@@ -0,0 +1,49 @@
+package ex06;
+
+class Person {
+    String name;
+
+    public Person() {
+
+    }
+
+    public Person(String theName) {
+        this.name = theName;
+    }
+}
+
+class EmployeeTest extends Person {
+    String id;
+
+    public EmployeeTest() {
+
+    }
+
+    public EmployeeTest(String name) {
+        super(name);
+    }
+
+    public EmployeeTest(String name, String id) {
+        super(name);
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeTest{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+public class Employee {
+    public static void main(String[] args) {
+        EmployeeTest e1 = new EmployeeTest("kim", "20210001");
+        System.out.println(e1);
+        EmployeeTest e2 = new EmployeeTest("kim");
+        System.out.println(e2);
+        EmployeeTest e3 = new EmployeeTest();
+        System.out.println(e3);
+    }
+}
