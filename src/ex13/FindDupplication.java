09239bc635d89d2495a81e38447c0289da190ed1
seoldonghun <ehdgns5647@naver.com>
2024-01-04 14:10:08 +0900

    map 연습

--- src/ex13/FindDupplication.java
+++ src/ex13/FindDupplication.java
@@ -7,7 +7,7 @@ public class FindDupplication {
         Set<String> s = new HashSet<>();
         String[] sample = {"사과", "사과", "바나나", "토마토"};
         for (String a : sample) {
-            if (!s.add(a)) {
+            if (!s.add(a)) { // 중복이 되면 set은 들어가지 않으니 add로 들어가지 않는 경우 false로 반환하니 가능한 코드이다.
                 System.out.println("중복된 단어: " + a);
             }
         }
